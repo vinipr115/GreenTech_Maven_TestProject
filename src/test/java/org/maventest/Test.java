66d41455148418b0980f29a9b9c611002534988e
vinipr115 <vinipr115@gmaail.com>
2025-04-21 19:17:20 +0530

    First Commit

--- src/test/java/org/maventest/Test.java
+++ src/test/java/org/maventest/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Test File");
+		//System.out.println("Test File");
+		System.out.println("Testing File");
 	}
 }
