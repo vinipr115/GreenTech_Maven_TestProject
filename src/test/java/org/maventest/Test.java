4c5419bfa33023d4673848d6926acea645984f83
vinipr115 <vinipr115@gmaail.com>
2025-04-22 16:02:21 +0530

    Praga's Code2

--- src/test/java/org/maventest/Test.java
+++ src/test/java/org/maventest/Test.java
@@ -6,5 +6,8 @@ public class Test {
 	public static void main(String[] args) {
 		//System.out.println("Test File");
 		System.out.println("Testing File");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 	}
 }
